186471272c788f87580c10d6fb7c1586fee5a30c
Mekelaina <truemr.derpster@gmail.com>
2020-06-28 18:53:06 -0400

    rendering for tile entities started but paused to update to avoid issues

--- src/main/java/com/mekelaina/gramarye/blocks/Charger.java
+++ src/main/java/com/mekelaina/gramarye/blocks/Charger.java
@@ -1,12 +1,16 @@
 package com.mekelaina.gramarye.blocks;
 
+import com.mekelaina.gramarye.blocks.tiles.ChargerTile;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
 
 public class Charger extends Block {
 
@@ -24,5 +28,16 @@ public class Charger extends Block {
     @Override
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.SOLID;
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new ChargerTile();
     }
 }
--- src/main/java/com/mekelaina/gramarye/setup/ClientSetup.java
+++ src/main/java/com/mekelaina/gramarye/setup/ClientSetup.java
@@ -3,12 +3,16 @@ package com.mekelaina.gramarye.setup;
 import com.mekelaina.gramarye.Gramarye;
 import com.mekelaina.gramarye.blocks.containers.ModContainers;
 import com.mekelaina.gramarye.blocks.screens.ObeliskScreen;
+import com.mekelaina.gramarye.blocks.tiles.ChargerTile;
+import com.mekelaina.gramarye.blocks.tiles.ModTileEntities;
+import com.mekelaina.gramarye.client.render.ChargerTileEntityRenderer;
 import com.mekelaina.gramarye.entities.DefaultSpellBoltRenderer;
 import com.mekelaina.gramarye.entities.SpellBoltEntity;
 
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -19,6 +23,8 @@ public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
         ScreenManager.registerFactory(ModContainers.OBELISK_CONTAINER.get(), ObeliskScreen::new);
         //RenderingRegistry.registerEntityRenderingHandler(SpellBoltEntity.class, DefaultSpellBoltRenderer::new);
+
+        ClientRegistry.bindTileEntitySpecialRenderer(ChargerTile.class, new ChargerTileEntityRenderer());
     }
 
     @SubscribeEvent
